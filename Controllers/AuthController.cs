b7ab437914466e81b47e0b6c46b9b8752624301b
Jairoalperez <jairoalberto98@hotmail.com>
2025-03-24 11:37:26 -0400

    created the auth controller

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.EntityFrameworkCore;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using LoginTemplate_RestAPI.Models;
+using LoginTemplate_RestAPI.Helpers;
+
+namespace LoginTemplate_RestAPI.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public AuthController(AppDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
