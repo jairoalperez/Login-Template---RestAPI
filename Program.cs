675ac4f97edc9125853554e4b1ed5a4622f61541
Jairoalperez <jairoalberto98@hotmail.com>
2025-03-24 11:03:56 -0400

    added ignore cycles

--- Program.cs
+++ Program.cs
@@ -27,7 +27,12 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(connectionString)
 );
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+    });
+    
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
