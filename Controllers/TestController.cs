a7eab22f3c40e1e9aa61691fa56fb84665ca5843
Jairoalperez <jairoalberto98@hotmail.com>
2025-03-24 10:55:33 -0400

    added test database endpoint

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -20,5 +20,21 @@ public class TestController : ControllerBase
         return Ok(Messages.API.Working);
     }
 
-    
+    // TEST DATABASE ENDPOINT
+    [HttpGet("database")]
+    public async Task<IActionResult> TestDatabaseConnection()
+    {
+        try
+        {
+            var result = await _dbContext.Database.ExecuteSqlRawAsync("SELECT 1");
+            if (result == -1)
+                return Ok(Messages.Database.ConnectionSuccess);
+            
+            return Problem(Messages.Database.ConnectionFailed);
+        }
+        catch (Exception ex)
+        {
+            return Problem(Messages.Database.ConnectionFailed, ex.Message);
+        }
+    }
 }
